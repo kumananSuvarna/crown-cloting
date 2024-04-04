0ac408b2ad316fe42e2b06adcdb355beb246c08f
KUMANAN <kumanan7265@gmail.com>
2024-04-04 18:19:56 +0800

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Homepage from './Homepage.component';
+
+function App() {
+  return (
+    <div >
+
+      <Homepage />
+  
+    </div>
+  );
+}
+
+export default App;
